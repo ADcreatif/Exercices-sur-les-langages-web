db601890a09d46e3ba9373a1159b6922d1d52f15
Alan <contact@ad-creatif.com>
2017-05-11 00:43:00 +0200

    Adding error template to exceptions

--- exception/exceptions.php
+++ exception/exceptions.php
@@ -2,19 +2,14 @@
 
 ini_set('display_errors', 0);
 
-echo '<meta charset="utf-8">';
 try{
 	// on essai de faire des trucs ici
-
-	if( !mail('alan@3wa.fr','salut') ){
+	if( !mail('wrong@mail.com','sujet') ){
 		throw new DomainException('Le mail n\'est pas parti');
 	}
 
-
 } catch(DomainException $e) {
-	echo "<p>Il y à eu un problème dans votre requête <strong>".$e->getMessage() ."</strong></p>";
-	
-	
-	// si ça ne marche pas on est renvoyé dans le catch
-	
+    // si ça ne marche pas on est renvoyé dans le catch
+    $error = $e->getMessage();
+    include('error_template.phtml');
 }
