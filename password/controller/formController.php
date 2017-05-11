54854d502e45c14b350ba7adf72cfd2e788de206
Alan <contact@ad-creatif.com>
2017-05-11 17:55:02 +0200

    Convert password project to MVC, adding salt encryption

---
+++ password/controller/formController.php
@@ -0,0 +1,19 @@
+<?php
+
+// on vérifie que le formulaire est envoyé
+if(array_key_exists('login', $_POST)){
+
+    // on extrait les variables du formulaire
+    extract($_POST);
+    // $pseudo = 'Francky';
+    // $password = 'Vincent';
+
+    // enfin on vérifie que le mot de passe est correct
+    try{
+        $password = new Password();
+        $password->checkPassword($pseudo, $pass);
+
+    } catch(DomainException $error){
+        $errors = $error->getMessage();
+    }
+}
--- password/index.php
+++ password/index.php
@@ -1,23 +1,7 @@
 <?php
-include 'password.php';
+include 'model/password.php';
+$errors = "";
 
-/*
-addPassword('zobby','lamouche');
-addPassword('fresd','aster');
-addPassword('francky','vincent');
-*/
-
-if(array_key_exists('login', $_POST)){
-    $pseudo = $_POST['pseudo'];
-    $pass = $_POST['password'];
-
-    checkPassword($pseudo, $pass);
-}
-
-?>
-
-<form method="POST">
-    <input type="text" name="pseudo" placeholder="pseudo">
-    <input type="password" name="password" placeholder="pass">
-    <input type="submit" name="login" value="connexion">
-</form>
+$page = 'form';                                 // nom de la page à afficher souvent $_GET['page'];
+include 'controller/'.$page.'Controller.php';   // chargement du controller
+include 'view/'.$page.'View.phtml';             // chargement de la vue
