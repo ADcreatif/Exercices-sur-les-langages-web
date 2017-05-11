b4d40bb722192fc5eb3dbcc601cf257907f59685
Alan <contact@ad-creatif.com>
2017-05-11 19:47:56 +0200

    update ajax_nav : message management

--- ajax_nav/ajax.php
+++ ajax_nav/ajax.php
@@ -1,6 +1,41 @@
 <?php
 
+function getMessages() {
+    $file = 'messages.csv';
+    $csv = fopen($file, 'r');
 
+    $messages = [];
+
+    while ($message = fgetcsv($csv)){
+        array_push($messages, $message[1]);
+    }
+    fclose($csv);
+
+    return $messages;
+}
+
+// gestion de la navigation
 if(array_key_exists('href',$_GET)){
-	include 'pages/'.$_GET['href'].'.phtml';
+
+    $page = $_GET['href'];
+
+    // on récupère les éventuelles variables du template
+    switch($page){
+        case 'message':
+            $messages = getMessages();
+            break;
+    }
+
+    //on affiche le template
+	include "pages/$page.phtml";
+}
+
+// gestion du formulaire
+if(array_key_exists('form',$_POST)){
+    $file = 'messages.csv';
+
+    $csv = fopen($file,'a');
+    fputcsv($csv, [$_POST['name'] , $_POST['message']]);
+
+    fclose($csv);
 }
--- ajax_nav/script.js
+++ ajax_nav/script.js
@@ -1,27 +1,55 @@
-$(function(){	
-	function onClickLink(event) {
-		
-		// récupération de l'attribut href du lien
-		var data = 'href=' + this.dataset.href;
-		
-		
-		// envoi de la requête ajax avec la méthode get
-		$.get('ajax.php', data, function(reponse){
-			// réponse php en cas de succès
-			var main = $('main');
-			
-			// on masque le contenu du main
-			main.fadeOut('slow',function(){
-				// quand l'animation est terminée on le réaffiche avec le contenu de la réponse php
-				main.empty().append(reponse).fadeIn();
-			})
-			
-		});		
+"use strict";
 
-		// inibition du comportement du lien par d�faut
-		event.preventDefault();
-	}			
+/**
+ * Cette fonction récupère et affiche le contenu de la page demandé
+ * @param pageName string nom de la vue à charger
+ */
+function changePage(pageName){
+    // récupération de l'attribut href du lien
+    var data = 'href=' + pageName;
+
+    // envoi de la requête ajax avec la méthode get
+    $.get('ajax.php', data, function(reponse){
+        // réponse php en cas de succès
+        var main = $('main');
+
+        // on masque le contenu du main
+        main.fadeOut('slow',function(){
+            // quand l'animation est terminée on le réaffiche avec le contenu de la réponse php
+            main.empty().append(reponse).fadeIn();
+        })
+    });
+}
+
+var onSubmitForm = function(event){
+    var data;
+
+    data = 'form=1&' + $(this).serialize();
+
+    $.post('ajax.php', data, function(reponse){
+        console.log('success', reponse);
+    });
+
+    event.preventDefault();
+};
+
+var onClickLink = function(event) {
+    // on lance la fonction de changement de page
+    changePage(this.dataset.href);
+
+    // inibition du comportement du lien par défaut
+    event.preventDefault();
+};
+
+// attendre que le DOM soit chargé
+$(function(){
+
+	// affichage d'une page par défaut
+    changePage('home');
 
 	// écouteur d'événement sur tous les liens de la page
 	$('a').click(onClickLink);
+
+	// grace à la métode on, on peut mettre un écouteur qui n'existe pas encore dans le DOM (le formailaire)
+    $('main').on('submit','form',onSubmitForm);
 });
